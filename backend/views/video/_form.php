70a6761cad3d7c9d4cce15994f8a81b949bcc5ef
Aldair Cruz Mateo <acruz@cias.local>
2024-02-22 11:36:59 -0600

    video listing page done

--- backend/views/video/_form.php
+++ backend/views/video/_form.php
@@ -6,6 +6,8 @@ use yii\bootstrap4\ActiveForm;
 /** @var yii\web\View $this */
 /** @var common\models\Video $model */
 /** @var yii\bootstrap4\ActiveForm $form */
+
+\backend\assets\TagsInputAsset::register($this);
 ?>
 
 <div class="video-form">
@@ -28,7 +30,9 @@ use yii\bootstrap4\ActiveForm;
                 </div>
             </div>
 
-            <?= $form->field($model, 'tags')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'tags', [
+                    'inputOptions' => ['data-role' => 'tagsinput']
+            ])->textInput(['maxlength' => true]) ?>
         </div>
         
         <div class="col-sm-4">
--- backend/views/video/index.php
+++ backend/views/video/index.php
@@ -26,15 +26,22 @@ $this->params['breadcrumbs'][] = $this->title;
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
-            'video_id',
-            'title',
-            'description:ntext',
-            'tags',
-            'status',
+            [
+                    'attribute' => 'video_id',
+                'content' => function($model){
+                    return $this->render('_video_item',['model' => $model]);
+                }
+            ],
+            [
+                    'attribute' => 'status',
+                'content' => function($model){
+                    return $model -> getStatusLabels()[$model->status];
+                }
+            ],
             //'has_thumbnail',
             //'video_name',
-            //'created_at',
-            //'updated_at',
+            'created_at:datetime',
+            'updated_at:datetime',
             //'created_by',
             [
                 'class' => ActionColumn::className(),
